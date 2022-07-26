db1afe0f978787c99a09f7d9d2f5a82bbbc4dada
Rene Collins <107972484+ReneTheMC@users.noreply.github.com>
2022-07-26 18:41:09 -0500

    1st edition

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RiE-Game-
-RiE is an open source music development game 
+
+RiE is an open source music development game made to learn music concepts and reimagine new ways to learn
+
+## Concepts
+
+-[ ] develop a guitar hero type game
+-[ ] use a Dance, Dance Revolution type control style
+-[ ] use popular music for user buy in
+-[ ]
---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+
+// =============
+
+
+// EVENT LISTENERS
+
+
+
+
+// ===============SETUP FOR CANVAS RENDERING =========//
+
+// ============ ENTITIES ============//
+
+
+
+// =============HELPER FUNCTIONS=========//
+
+
+
+
+// GUI
+
+
+// KEYBOARD INTERACTION LOGIC
+
+
+
+// ================ GAME PROCESSES ===========//
+
+
+
+// ============= 
